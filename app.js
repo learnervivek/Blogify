cc2f0a2e1caf5a1ae13ca20262ecf41d279eb560
Vivek Kumar Gupta <v8381813340@gmail.com>
2026-01-14 00:35:49 +0530

    fix bugs:latest bugs at top

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(checkForAuthenticationCookie("token"));
 app.use(express.static(path.resolve("./public")));
 
 app.get("/", async (req, res) => {
-  const allBlogs = await Blog.find({});
+  const allBlogs = await Blog.find({}).sort({ createdAt: -1 });
   res.render("home", {
     user: req.user,
     blogs: allBlogs,
